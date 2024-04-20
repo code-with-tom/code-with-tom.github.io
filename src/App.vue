0e8c20189b7764f4aa91ea6e3344b3127b330be9
code-with-tom <nadi.thomas.pro@gmail.com>
2024-04-20 16:11:18 +0200

    remove boilerplate code to start clean

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,5 @@
-<script setup>
-import {onMounted, ref} from 'vue'
-
-const placeholder = ref({})
-
-onMounted( async () => {
-  const response = await fetch("https://raw.githubusercontent.com/code-with-tom/code-with-tom.github.io-data/main/placeholder.json");
-  placeholder.value =  await response.json()
-})
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    Website maintenance
   </div>
-  <HelloWorld msg="Vite + Vue(autodeploy)" />
-  {{ placeholder }}
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
