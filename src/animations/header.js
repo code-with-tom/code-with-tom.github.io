baadd6ce636fe7a99fc041503aafd38c8f135eaf
code-with-tom <nadi.thomas.pro@gmail.com>
2024-04-24 19:03:27 +0200

    responsive navbar and animations

--- src/animations/header.js
+++ src/animations/header.js
@@ -1,21 +1,85 @@
 import { gsap } from "gsap"
 
-const animateLogo = () => {
-  const logo = document.querySelector('.anim-logo')
-  gsap.set(logo, { opacity: 0 });
 
-  gsap.to(logo, { opacity: 1, duration: 0.5, ease: "power2.inOut" });
-}
+const animateInitDesktopNavbar = () => {
+  const navbarLogo = document.querySelector('.anim-navbar-logo');
+  const menu = document.querySelector('.anim-menu');
+  const menuLinks = document.querySelectorAll('.anim-menu-link');
 
-const animateMenu = () => {
-  const menuItems = document.querySelectorAll('.anim-menu-item');
-  gsap.set(menuItems, { opacity: 0 });
+  const timeline = gsap.timeline();
 
-  menuItems.forEach((menuItem, index) => {
-    console.log('pol');
-    gsap.to(menuItem, { opacity: 1, duration: 1, delay: index * 0.2, ease: "power2.inOut" });
+  timeline.set(navbarLogo, { x: '-100%', opacity: 0 })
+  timeline.set(menu, { x: '-100%', opacity: 0 })
+  timeline.set(menuLinks, {  opacity: 0 })
+
+  timeline.to(navbarLogo, { x: 0, opacity: 1, duration: 1, ease: 'power2.out' }, 'start')
+  timeline.to(menu, { x: 0, opacity: 1, duration: 1, ease: 'power2.out' }, 'start')
+
+  menuLinks.forEach((menuLinks, index) => {
+    timeline.to(menuLinks, { opacity: 1,  duration: .1,  delay: index * 0.001, ease: "power2.inOut" }, 'start');
   });
 }
 
-export { animateLogo, animateMenu }
+const animateInitMobileNavbar = () => {
+  const mobileNavbarLogo = document.querySelector('.anim-mobile-navbar-logo');
+  const mobileNavbarBurger = document.querySelector('.anim-mobile-navbar-burger');
+
+  const timeline = gsap.timeline();
+
+  timeline.set(mobileNavbarLogo, { x: '-100%', opacity: 1 })
+  timeline.set(mobileNavbarBurger, { x: '100%', opacity: 1 })
+
+  timeline.to(mobileNavbarLogo, { x: 0, opacity: 1, duration: .5, ease: 'power2.out' }, 'start')
+  timeline.to(mobileNavbarBurger, { x: 0, opacity: 1, duration: 1, ease: 'power2.out' }, 'start')
+}
+
+const animateOpenMobileBurgerMenu = () => {
+  const mobileMenuPanel = document.querySelector('.anim-mobile-menu-panel');
+  const mobileMenuPanelOverlay = document.querySelector('.anim-mobile-menu-panel-overlay');
+  const mobileMenuIconBase = document.querySelector('.anim-mobile-menu-icon-base');
+  const mobileMenuIconClose = document.querySelector('.anim-mobile-menu-icon-close');
+  const mobileMenuLinks = document.querySelectorAll('.anim-mobile-menu-link');
+  
+  const timeline = gsap.timeline();
+
+  timeline.set(mobileMenuIconClose, { scale: 0 })
+  timeline.set(mobileMenuLinks, { x: '110%', opacity: '0' })
+
+  timeline.to(mobileMenuIconBase, { scale: 0, display: 'none', duration: 0.15, ease: 'power2.out' })
+  timeline.to(mobileMenuIconClose, { scale: 1, display: 'block', duration: 0.15, ease: 'power2.out' })
+  timeline.to(mobileMenuPanel, { y: 0, duration: 0.5, ease: 'power2.out' })
+
+  mobileMenuLinks.forEach((mobileMenuLink, index) => {
+    timeline.to(mobileMenuLink, { x: 0, opacity: 1, duration: 0.15, delay: index * 0.001, ease: "power2.inOut" });
+  });
+
+  timeline.to(mobileMenuPanelOverlay, { backgroundColor: 'rgba(0, 0, 0, 0.5)', duration: 0.5, ease: 'power2.out' })
+}
+
+const animateCloseMobileBurgerMenu = () => {
+  const mobileMenuPanel = document.querySelector('.anim-mobile-menu-panel');
+  const mobileMenuPanelOverlay = document.querySelector('.anim-mobile-menu-panel-overlay');
+  const mobileMenuIconBase = document.querySelector('.anim-mobile-menu-icon-base');
+  const mobileMenuIconClose = document.querySelector('.anim-mobile-menu-icon-close');
+  const mobileMenuLinks = document.querySelectorAll('.anim-mobile-menu-link');
+
+  const timeline = gsap.timeline();
+
+  timeline.to(mobileMenuIconClose, { scale: 0, display: 'none', duration: .15, ease: 'power2.out' })
+  timeline.to(mobileMenuIconBase, { scale: 1, display: 'block', duration: .15, ease: 'power2.out' })
+
+  mobileMenuLinks.forEach((mobileMenuLink, index) => {
+    timeline.to(mobileMenuLink, { x: '110%', opacity: 0, duration: 0.15, delay: index * 0.001, ease: "power2.inOut" });
+  });
+
+  timeline.to(mobileMenuPanelOverlay, { backgroundColor: 'rgba(0, 0, 0, 1)', duration: 0.5, ease: 'power2.out' })
+  timeline.to(mobileMenuPanel, { y: "-100%", duration: 0.5, ease: 'power2.in'});
+}
+
+export { 
+  animateInitDesktopNavbar,
+  animateInitMobileNavbar,
+  animateOpenMobileBurgerMenu,
+  animateCloseMobileBurgerMenu 
+}
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,135 +1,190 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { animateLogo, animateMenu } from '@animations/header';
-import { animateHeroSection, animateHeroBlob, animateHeroIcons} from '@animations/heroSection';
 
-const sections = ref([]);
-const current = ref("home")
+import { 
+  animateInitDesktopNavbar,
+  animateInitMobileNavbar,
+  animateOpenMobileBurgerMenu,
+  animateCloseMobileBurgerMenu 
+} from '@animations/header';
+
+const mobileMenu = ref(false)
+
+const toggleMobileMenu = () => {
+  mobileMenu.value = !mobileMenu.value;
+
+  if(mobileMenu.value) {
+    animateOpenMobileBurgerMenu()
+    // document.body.style.overflow = 'hidden'
+  } else {
+    animateCloseMobileBurgerMenu()
+    // document.body.style.overflow = 'auto'
+  }
+}
 
 onMounted(() => {
-  sections.value = document.querySelectorAll('section')
+  animateInitDesktopNavbar()
+  animateInitMobileNavbar()
+})
 
-  window.addEventListener('wheel', handleScroll, { passive: false })
-  scrollToSection(sections.value[0]);
-  animateHeroSection()
-  animateLogo()
-  animateMenu()
-  animateHeroBlob()
-  animateHeroIcons()
+// import { ref, onMounted } from 'vue'
+// import { animateLogo, animateMenu } from '@animations/header';
+// import { animateHeroSection, animateHeroBlob, animateHeroIcons} from '@animations/heroSection';
 
-});
+// const sections = ref([]);
+// const current = ref("home")
 
-const handleScroll = (event) => {
-  event.preventDefault()
+// onMounted(() => {
+  // sections.value = document.querySelectorAll('section')
+  // window.addEventListener('wheel', handleScroll, { passive: false })
+  // scrollToSection(sections.value[0]);
+  // animateHeroSection()
+  // animateLogo()
+  // animateMenu()
+  // animateHeroBlob()
+  // animateHeroIcons()
+// });
 
-  if (sections.value.length === 0) return
+// const handleScroll = (event) => {
+//   event.preventDefault()
 
-  const currentSection = document.querySelector('.active');
-  const currentIndex = Array.from(sections.value).indexOf(currentSection);
-  const nextIndex = currentIndex + (event.deltaY > 0 ? 1 : -1);
+//   if (sections.value.length === 0) return
 
-  if (nextIndex >= 0 && nextIndex < sections.value.length) {
-    scrollToSection(sections.value[nextIndex]);
-  }
-}
+//   const currentSection = document.querySelector('.active');
+//   const currentIndex = Array.from(sections.value).indexOf(currentSection);
+//   const nextIndex = currentIndex + (event.deltaY > 0 ? 1 : -1);
 
-const scrollToSection = (section) => {
-  current.value = section.getAttribute("name")
+//   if (nextIndex >= 0 && nextIndex < sections.value.length) {
+//     scrollToSection(sections.value[nextIndex]);
+//   }
+// }
 
-  if(section.getAttribute("name") === 'home') {
+// const scrollToSection = (section) => {
+//   current.value = section.getAttribute("name")
+
+//   if(section.getAttribute("name") === 'home') {
     
-  }
-  else if(section.getAttribute("name") === 'services') {
-    // test1('.test-1')
-  }
-  else if(section.getAttribute("name") === 'portfolio') {
-    // test1('.test-2')
-  }
-  else if(section.getAttribute("name") === 'blog') {
-    // test1('.test-3')
-  }
-  else if(section.getAttribute("name") === 'about') {
-    // test1('.test-4')
-  }
-  else if(section.getAttribute("name") === 'contact') {
-    // test1('.test-5')
-  }
+//   }
+//   else if(section.getAttribute("name") === 'services') {
+//     // test1('.test-1')
+//   }
+//   else if(section.getAttribute("name") === 'portfolio') {
+//     // test1('.test-2')
+//   }
+//   else if(section.getAttribute("name") === 'blog') {
+//     // test1('.test-3')
+//   }
+//   else if(section.getAttribute("name") === 'about') {
+//     // test1('.test-4')
+//   }
+//   else if(section.getAttribute("name") === 'contact') {
+//     // test1('.test-5')
+//   }
 
-  const targetScrollPosition = section.offsetTop;
-  const startScrollPosition = window.pageYOffset;
-  const distance = targetScrollPosition - startScrollPosition;
-  const duration = 400; // Adjust duration as needed
+//   const targetScrollPosition = section.offsetTop;
+//   const startScrollPosition = window.pageYOffset;
+//   const distance = targetScrollPosition - startScrollPosition;
+//   const duration = 400; // Adjust duration as needed
 
-  let startTime;
+//   let startTime;
 
-  const scrollAnimation = (currentTime) => {
+//   const scrollAnimation = (currentTime) => {
    
-    if (!startTime) startTime = currentTime;
-    const timeElapsed = currentTime - startTime;
-    const scrollProgress = Math.min(timeElapsed / duration, 1);
-    const scrollPosition = startScrollPosition + distance * scrollProgress;
-    window.scrollTo(0, scrollPosition);
+//     if (!startTime) startTime = currentTime;
+//     const timeElapsed = currentTime - startTime;
+//     const scrollProgress = Math.min(timeElapsed / duration, 1);
+//     const scrollPosition = startScrollPosition + distance * scrollProgress;
+//     window.scrollTo(0, scrollPosition);
 
-    if (timeElapsed < duration) {
-      requestAnimationFrame(scrollAnimation);
-    } else {
-      sections.value.forEach((sec) => {
-        sec.classList.remove('active');
-      });
-      section.classList.add('active');
-    }
-  }
+//     if (timeElapsed < duration) {
+//       requestAnimationFrame(scrollAnimation);
+//     } else {
+//       sections.value.forEach((sec) => {
+//         sec.classList.remove('active');
+//       });
+//       section.classList.add('active');
+//     }
+//   }
 
-  requestAnimationFrame(scrollAnimation);
-}
+//   requestAnimationFrame(scrollAnimation);
+// }
 </script>
 
 <template>
-  <!-- NAVBAR DESKTOP -->
-  <nav class="hidden md:block sticky text-white h-[80px]  w-full  top-0 z-50">
+  <nav class="hidden md:block sticky text-white h-[80px] w-full top-0 z-50">
     <div class="mx-auto max-w-screen-lg h-full px-4 lg:px-0">
       <div class="flex justify-between items-center h-full">
         <div class="select-none">
-          <div class="anim-logo flex space-x-1 oswald uppercase">
+          <div class="anim-navbar-logo flex space-x-1 oswald uppercase ">
             <div>Code</div>
             <div>With</div>
             <div>Tom</div>
           </div>
         </div>
-        <div>
-          <ul class=" oswald uppercase ">
+        <div class="anim-menu">
+          <ul class="oswald uppercase">
             <li class="space-x-6">
-              <a class="anim-menu-item cursor-pointer select-none" :class="current === 'home' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[0])">Home</a>
-              <a class="anim-menu-item cursor-pointer select-none" :class="current === 'services' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[1])">Services</a>
-              <a class="anim-menu-item cursor-pointer select-none" :class="current === 'portfolio' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[2])">Portfolio</a>
-              <a class="anim-menu-item cursor-pointer select-none" :class="current === 'blog' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[3])">Blog</a>
-              <a class="anim-menu-item cursor-pointer select-none" :class="current === 'about' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[4])">About</a>
-              <a class="anim-menu-item cursor-pointer select-none" :class="current === 'contact' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[5])">Contact</a>
+              <a class="anim-menu-link cursor-pointer select-none"  @click="scrollToSection(sections[0])">Home</a>
+              <a class="anim-menu-link cursor-pointer select-none"  @click="scrollToSection(sections[1])">Services</a>
+              <a class="anim-menu-link cursor-pointer select-none"  @click="scrollToSection(sections[2])">Portfolio</a>
+              <a class="anim-menu-link cursor-pointer select-none"  @click="scrollToSection(sections[3])">Blog</a>
+              <a class="anim-menu-link cursor-pointer select-none"  @click="scrollToSection(sections[4])">About</a>
+              <a class="anim-menu-link cursor-pointer select-none"  @click="scrollToSection(sections[5])">Contact</a>
             </li>
           </ul>
         </div>
       </div>
     </div>
   </nav>
+ 
+  <nav class="block md:hidden sticky text-white h-[80px] w-full top-0 z-50 bg-black border-b border-purple-600 border-opacity-25">
+    <div class="h-full px-4">
+      <div class="flex justify-between items-center h-full  bg-black relative z-50">
+        <div class="select-none">
+          <div class="anim-mobile-navbar-logo flex space-x-1 oswald uppercase">
+            <div>Code</div>
+            <div>With</div>
+            <div>Tom</div>
+          </div>
+        </div>
+        <div @click="toggleMobileMenu()" class="anim-mobile-navbar-burger h-[80px] w-[80px] flex justify-end items-center">
+          <img  class="anim-mobile-menu-icon-base w-8 h-8" src="@assets/icons/menu-right.png" alt="">
+          <img  class="anim-mobile-menu-icon-close hidden w-8 h-8" src="@assets/icons/menu-close.png" alt="">
+        </div>
+      </div>
+      <div class="anim-mobile-menu-panel block relative z-0 -translate-y-full">
+        <ul class="oswald uppercase">
+          <li class="anim-mobile-menu-panel-overlay flex flex-col bg-black -mx-4 p-4 backdrop-filter backdrop-blur-xl border-b border-purple-600 border-opacity-25
+              divide-y-2 divide-purple-600 divide-opacity-10">
+            <a class="anim-mobile-menu-link cursor-pointer select-none py-4" :class="current === 'home' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[0])">Home</a>
+            <a class="anim-mobile-menu-link cursor-pointer select-none py-4" :class="current === 'services' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[1])">Services</a>
+            <a class="anim-mobile-menu-link cursor-pointer select-none py-4" :class="current === 'portfolio' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[2])">Portfolio</a>
+            <a class="anim-mobile-menu-link cursor-pointer select-none py-4" :class="current === 'blog' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[3])">Blog</a>
+            <a class="anim-mobile-menu-link cursor-pointer select-none py-4" :class="current === 'about' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[4])">About</a>
+            <a class="anim-mobile-menu-link cursor-pointer select-none py-4" :class="current === 'contact' ? 'text-purple-600' : 'text-white'" @click="scrollToSection(sections[5])">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-  <!-- SECTIONS -->
   <div class=" text-white">
-    <section id="bg-hero" name="home" class="py-20 bg-black h-screen md:-mt-[80px] relative overflow-hidden">
+    <section name="home" class="py-20 bg-black h-screen -mt-[80px] relative overflow-hidden">
       <ul class="absolute h-screen w-screen top-0 left-0 bottom-0 right-0 filter blur-[128px] overflow-hidden">
         <li class="anim-hero-blob absolute bg-purple-600 bg-opacity-25 h-[400px] w-[400px] right-0 bottom-0"></li>
         <li class="anim-hero-blob absolute bg-purple-600 bg-opacity-25 h-[300px] w-[300px] left-0 top-0"></li>
       </ul>
-      <div class="relative test-0 oswald h-full w-full flex flex-col justify-center mx-auto max-w-screen-lg  px-4 lg:px-0  ">
-        <div class="anim-hero-item text-sm text-purple-600 uppercase">
+      <div class="relative test-0 oswald h-full w-full flex flex-col justify-center mx-auto max-w-screen-lg px-4 lg:px-0">
+        <div class="anim-hero-item text-purple-600 uppercase">
           Crafting Digital Solutions with Precision
         </div>
-        <div class="anim-hero-item text-8xl mt-2">
+        <div class="anim-hero-item text-5xl md:text-8xl mt-2">
           I’M THOMAS, DEVELOPER FROM FRANCE
         </div>
-        <div class="anim-hero-item text-lg mt-8 w-[700px]">
+        <div class="anim-hero-item text-2xl md:text-xl mt-8 md:w-[700px]">
           Welcome to my digital playground. Experience the fusion of creativity and technology. Dive into my portfolio and explore my world 
         </div>
-        <div class="absolute bottom-0 w-full anim-hero-icons">
+        <div class="absolute bottom-0 w-full hidden">
           <ul class="flex space-x-12">
             <li class="relative w-14 h-14 anim-hero-images">
               <img class="absolute left-0 h-full w-full" src="@assets/icons/html.png" alt="">
